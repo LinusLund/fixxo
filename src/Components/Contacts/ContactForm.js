d51e3f7ea893d6c0ac80c477a8b276f786223c03
LinusLund <lundlinus@live.se>
2022-10-27 14:29:06 +0200

    försöker koda en valideringsmetod för varje fält, samt en som kallar på de andra

--- src/Components/Contacts/ContactForm.js
+++ src/Components/Contacts/ContactForm.js
@@ -4,78 +4,98 @@ const ContactForm = () => {
     const [contactForm, setContactForm] = useState({name: '', email:'', comment:''})
     const [formErrors, setFormErrors] = useState({})
     const[canSubmit, setCanSubmit] = useState()
-    const[canValidate, setValidation] = useState()
-   
-   
+    
+    let inputName = 'validSuccessName'
+        if(formErrors.name)
+            inputName = 'validFailName'
 
-    const validate = (values) => {
+    let inputEmail = 'validSuccessEmail'
+        if(formErrors.email)
+            inputName ='validFailEmail'
+
+    let inputComment = 'validSuccessComment'
+            if(formErrors.comment)
+                inputName ='validFailComment'
+
+
+    const validateName = (nameValidation) => {
+        const errors = {}
+        const regex_name = /^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]+$/u
+            if(!nameValidation.name)
+                errors.name = "You must enter a name"
+    
+
+            else if(nameValidation.name.length < 2)
+                errors.name = "Your Name must be atleast 2 characters long"
+        
+
+            else if(!regex_name.test(nameValidation.name))
+            errors.name = "Please use letters and ',.'-' these characters only "
+
+            return errors
+    }
+
+    const validateEmail = (emailValidation) => {
         const errors = {}
         const regex_email = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
-        const regex_name = /^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]+$/u
-
-        if(!values.name)
-            errors.name = "You must enter a name"
- 
-
-        else if(values.name.length < 2)
-            errors.name = "Your Name must be atleast 2 characters long"
-     
-
-        else if(!regex_name.test(values.name))
-           errors.name = "Please use letters and ',.'-' these characters only "
-       
         
-        if(!values.email)
+        if(!emailValidation.email)
             errors.email = "You must enter an email"
-         
-
-        else if (!regex_email.test(values.email))
+     
+        else if (!regex_email.test(emailValidation.email))
             errors.email = "You must enter a valid e-mail address (eg. name@domain.com)"
-           
 
-        if(!values.comment)
+            return errors
+    }
+
+    const validateComment = (commentValidation) => {
+        const errors = {}
+        if(!commentValidation.comment)
             errors.comment = "You must enter a comment"
-           
+       
 
-        else if(values.comment.length < 5)
+        else if(commentValidation.comment.length < 5)
             errors.comment = "Your comment must be atleast five characters long"
-           
-        
-        if(Object.keys(errors).length === 0)
+
+            return errors
+    }
+
+    const handleValidationData =(values) => {
+            validateName(values.nameValidation)
+            validateEmail(values.emailValidation)
+            validateComment(values.commentValidation)
+
+        if(Object.keys(values).length === 0)
             setCanSubmit(true)
         else
             setCanSubmit(false)
 
-
-        return errors;
+        
     }
 
     const handleChange = (e) => {
         e.preventDefault()
         const {id, value} = e.target
-        setContactForm({...contactForm, [id]: value})
+            setContactForm({...contactForm, [id]: value})
     }
 
     const handleKeyUp = (e) => {
-        setFormErrors(validate(contactForm))
+        setFormErrors(handleValidationData(formErrors))
         e.preventDefault()
     }
 
-    const handleSubmit = (e) => {
-        e.preventDefault()  
-        if(canSubmit === false)
-            console.log(formErrors);
-        else
-            console.log(contactForm)
-            
-        return contactForm
+    const handleSubmit= () => {
+        setFormErrors(handleValidationData(formErrors))  
+            if(canSubmit === false)
+                console.log(formErrors);
+            else
+                console.log(contactForm)
+                
      }
 
-    // const handleFocus = (canValidate) => setValidation({isFocused: true}) vill validera ett fält i taget, samt ändra border-color på input-fält när det finns errorMessage.
-        
+     
 
-    // const handleBlur = (canValidate) => setValidation({isFocused: false}) vill validera ett fält i taget, samt ändra border-color på input-fält när det finns errorMessage.
-          
+    
     return (
         <section className="commentForm">
             <div className="container">
@@ -83,21 +103,21 @@ const ContactForm = () => {
                         <h5>Come in contact with us</h5>
                         <form onSubmit={handleSubmit} noValidate>
                         <div>
-                            <input className = "failedValid" id="name" type="text"  onFocus={handleFocus} onBlur={handleBlur}  placeholder='Your Name' value={contactForm.name} onChange={handleChange} onKeyUp={handleKeyUp} />
+                            <input className ={inputName} id="name" type="text"     placeholder='Your Name' value={contactForm.name} onChange={handleChange} onKeyUp={handleKeyUp} />
                             <div className='errorMessage'>{formErrors.name}</div>    
                         </div>
                         <div>
-                            <input className = "failedValid" id="email" type="email"  placeholder='Your Mail' value={contactForm.email} onChange={handleChange} onKeyUp={handleKeyUp} />
+                            <input className={inputEmail} id="email" type="email"  placeholder='Your Mail' value={contactForm.email} onChange={handleChange} onKeyUp={handleKeyUp} />
                             <div className='errorMessage'>{formErrors.email}</div> 
                         </div>
 
                         <div className='textarea'>
-                            <textarea className = "failedValid" id='comment'  placeholder = "Comments" value= {contactForm.comment} onChange={handleChange} onKeyUp={handleKeyUp} ></textarea>
+                            <textarea className={inputComment} id='comment'  placeholder="Comments" value= {contactForm.comment} onChange={handleChange} onKeyUp={handleKeyUp}></textarea>
                             <div className='errorMessage'>{formErrors.comment}</div> 
                         </div>
 
                         <div>
-                            <button type ="submit" className="btn-submit">Post Comments</button>
+                            <button type="submit" className="btn-submit">Post Comments</button>
                         </div>
                     </form>
                 </>
