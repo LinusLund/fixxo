d88f65937e281ba35773225c588ea086fc77a3ff
LinusLund <lundlinus@live.se>
2022-10-28 11:51:21 +0200

    asdqweasd

--- src/Components/Contacts/ContactForm.js
+++ src/Components/Contacts/ContactForm.js
@@ -7,97 +7,118 @@ const ContactForm = () => {
     
 
 
-    const handleValidationData =(values) => {
 
-        validateName(values.nameValidation)
-        validateEmail(values.emailValidation)
-        validateComment(values.commentValidation)
 
-    if(Object.keys(values).length === 0)
-        setCanSubmit(true)
-    else
-        setCanSubmit(false)   
-
-    return formErrors;
-            }
-
-    const validateName = (nameValidation) => {
+    const validateName = (value) => {
         
         const regex_name = /^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]+$/u
-            if(!nameValidation.name)
-            nameValidation.name = "You must enter a name"
+
+            if(!value.name)
+            return"You must enter a name"
     
 
-            else if(nameValidation.name.length < 2)
-            nameValidation.name = "Your Name must be atleast 2 characters long"
+            else if(value.name.length < 2)
+            return"Your Name must be atleast 2 characters long"
         
 
-            else if(!regex_name.test(nameValidation.name))
-            nameValidation.name = "Please use letters and ',.'-' these characters only "
+            else if(!regex_name.test(value.name))
+            return "Please use letters and ',.'-' these characters only "
 
-            setFormErrors(formErrors)
-            
+        
+         return;   
     }
 
-    const validateEmail = (emvailValidation) => {
-                
+    const validateEmail = (value) => {
+               
         const regex_email = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
         
-        if(!emvailValidation.email)
-        emvailValidation.email = "You must enter an email"
+        if(!value.email)
+         return "You must enter an email"
      
-        else if (!regex_email.test(emvailValidation.email))
-        emvailValidation.email = "You must enter a valid e-mail address (eg. name@domain.com)"
+        else if (!regex_email.test(value.email))
+         return "You must enter a valid e-mail address (eg. name@domain.com)"
 
-        setFormErrors(formErrors)
         
-            
+        
+        return ;
     }
 
-    const validateComment = (commentValidation) => {
-        
-        if(!commentValidation.comment)
-        commentValidation.comment = "You must enter a comment"
+    const validateComment = (value) => {
+         
+        if(!value.comment)
+        return "You must enter a comment"
        
 
-        else if(commentValidation.comment.length < 5)
-        commentValidation.comment = "Your comment must be atleast five characters long"
+        else if(value.comment.length < 5)
+        return "Your comment must be atleast five characters long"
 
-            commentValidation(setFormErrors(formErrors)) 
+        return;
+
+
             
     }
+    
+    const handleValidationData =(data) => {
+        const  errors={}
 
-    let inputName = 'validSuccessName'
+        errors.name = validateName(data)
+        errors.email = validateEmail(data)
+        errors.comment = validateComment(data)
+   
+       if(Object.keys(errors).length === 0)
+           setCanSubmit(true)
+       else
+           setCanSubmit(false)
+        
+        return canSubmit;
+
+               }
+// ändrar klass på inputfälten baserat på huruvida det finns felmeddelande eller inte. Scss ändrar bordern om det finns fel.
+    let inputName = 'validSuccess'
         if(formErrors.name)
-        inputName = 'validFailName'
+        inputName = 'validFail'
 
-    let inputEmail = 'validSuccessEmail'
+    let inputEmail = 'validSuccess'
         if(formErrors.email)
-            inputName ='validFailEmail'
+            inputEmail ='validFail'
 
-    let inputComment = 'validSuccessComment'
+    let inputComment = 'validSuccess'
             if(formErrors.comment)
-                inputName ='validFailComment'
+                inputComment ='validFail'
 
-
+//fyller på contactForm med värden från inputfälten
     const handleChange = (e) => {
-        e.preventDefault()
+        
         const {id, value} = e.target
             setContactForm({...contactForm, [id]: value})
     }
-
-    const handleKeyUp = (e) => {
-        handleValidationData(setFormErrors(formErrors))
-        e.preventDefault()
+    //Spreadar formErrors och skriver över det valda fältet, ändrar inte de andra.
+    const handleKeyUpName = (e) => {
+        setFormErrors({...formErrors, name: validateName(contactForm)})
+        
     }
 
-    const handleSubmit= () => {
-        handleValidationData(setFormErrors(formErrors))  
+    const handleKeyUpEmail = (e) => {
+        setFormErrors({...formErrors, email: validateEmail(contactForm)})
+        
+    }
+
+    const handleKeyUpComment = (e) => {
+        setFormErrors({...formErrors, comment: validateComment(contactForm)})
+        
+    }
+    //kallar handleValidation data att köra valideringen med allt som finns i contactForm.
+    //därefter skriver den antingen ut formErrors eller contactForm beroende på status på canSubmit.
+
+    const handleSubmit= (e) => {
+        handleValidationData(contactForm) 
+        e.preventDefault()
             if(canSubmit === false)
                 console.log(formErrors);
             else
                 console.log(contactForm)
-                
+        
+             return  ;
      }
 
      
@@ -106,20 +127,28 @@ const ContactForm = () => {
     return (
         <section className="commentForm">
             <div className="container">
-                <>
+            {
+
+             canSubmit?
+
+                     (<div class="container-sm style=d-flex">Thank you for your comment!</div>)
+                    
+                 :
+                 (  
+                    <>
                         <h5>Come in contact with us</h5>
                         <form onSubmit={handleSubmit} noValidate>
                         <div>
-                            <input className ={inputName} id="name" type="text"     placeholder='Your Name' value={contactForm.name} onChange={handleChange} onKeyUp={handleKeyUp} />
+                            <input className ={inputName} id="name" type="text" placeholder='Your Name' value={contactForm.name} onChange={handleChange} onKeyUp={handleKeyUpName} />
                             <div className='errorMessage'>{formErrors.name}</div>    
                         </div>
                         <div>
-                            <input className={inputEmail} id="email" type="email"  placeholder='Your Mail' value={contactForm.email} onChange={handleChange} onKeyUp={handleKeyUp} />
+                            <input className={inputEmail} id="email" type="email" placeholder='Your Mail' value={contactForm.email} onChange={handleChange} onKeyUp={handleKeyUpEmail} />
                             <div className='errorMessage'>{formErrors.email}</div> 
                         </div>
 
                         <div className='textarea'>
-                            <textarea className={inputComment} id='comment'  placeholder="Comments" value= {contactForm.comment} onChange={handleChange} onKeyUp={handleKeyUp}></textarea>
+                            <textarea className={inputComment} id='comment' placeholder="Comments" value= {contactForm.comment} onChange={handleChange} onKeyUp={handleKeyUpComment}></textarea>
                             <div className='errorMessage'>{formErrors.comment}</div> 
                         </div>
 
@@ -128,6 +157,9 @@ const ContactForm = () => {
                         </div>
                     </form>
                 </>
+                 )
+            }         
+             
         </div>
     </section> 
 
