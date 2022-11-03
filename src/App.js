6dccfdcf00518323a5dc119ee85820b8438ca817
LinusLund <lundlinus@live.se>
2022-11-03 08:42:56 +0100

    nu funkar det att hämta produkter(om man trycker F5 några gånger)

--- src/App.js
+++ src/App.js
@@ -38,7 +38,7 @@ const fetchFeaturedProducts = async () => {
 
  }
 fetchFeaturedProducts()
-},[setProducts])
+},[])
 
 
 
--- src/Components/ProductCard.js
+++ src/Components/ProductCard.js
@@ -6,28 +6,17 @@ import { NavLink } from 'react-router-dom'
 
 const ProductCard = ({product}) => {
 
-    const addToWishList = (e) => {
-        console.log (`${e.product}added to wish list`)
-    }
-
-    const addToCompare = (e) => {
-        console.log ("added to Compare list")
-    }
-
-    const addToCart = (e) => {
-        console.log ("added to Shopping Cart")
-    }
-
+ 
   return (
 
     <div className="col">
         <div className="card">
-            <div className="img">
-                <img src={product.img} alt= {product.name} />
+            <div className="img border">
+                <img src={product.imageName} alt= {product.name} />
                 <div className="card-menu">
-                    <button onClick={addToCompare} className = 'menu-link'><i className="fa-regular fa-code-compare"></i></button>
-                    <button onClick={addToWishList(product.id)} className = 'menu-link'><i className="fa-regular fa-heart"></i></button>
-                    <button onClick={addToCart} className = 'menu-link'><i className="fa-regular fa-bag-shopping"></i></button>
+                    <button  className = 'menu-link'><i className="fa-regular fa-code-compare"></i></button>
+                    <button  className = 'menu-link'><i className="fa-regular fa-heart"></i></button>
+                    <button  className = 'menu-link'><i className="fa-regular fa-bag-shopping"></i></button>
                 </div>
 
                 <NavLink to={`/products/${product.name.toLowerCase().replace(/ /gi,"-")}`} className="QuickButton">
